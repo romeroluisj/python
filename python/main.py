a2c2fa94c9f29e5d42494d1194845be6143855bd
Luis J. Romero <romeroluisj@gmail.com>
2021-12-21 05:55:15 -0500

    testing .gitignore

--- python/main.py
+++ python/main.py
@@ -3,20 +3,7 @@ from numbers_parser import Document
 
 
 def main():
-    stock = Stock()
-    stock.set_ticker("Bac")
-    print(stock.get_last_dividend())
-
-    doc = Document("../docs/Example.numbers")
-    sheets = doc.sheets()
-    tables = sheets["Sheet 1"].tables()
-    table = tables["Table 1"]
-
-    # row, column syntax
-    print("Cell C2 contains", table.cell(2, 1))
-    # Excel/Numbers-style cell references
-    print("Cell C2 contains", table.cell("B3"))
-    print("Cell C2 contains", table.cell("C2"))
+    print("hello, world\n")
 
 
 if __name__ == '__main__':
--- python/stock.py
+++ python/stock.py
@@ -1,6 +1,25 @@
 import yfinance as yf
 
 
+"""
+# Ex lines from main:
+    stock = Stock()
+    stock.set_ticker("Bac")
+    print(stock.get_last_dividend())
+
+    doc = Document("../docs/Example.numbers")
+    sheets = doc.sheets()
+    tables = sheets["Sheet 1"].tables()
+    table = tables["Table 1"]
+
+    # row, column syntax
+    print("Cell C2 contains", table.cell(2, 1))
+    # Excel/Numbers-style cell references
+    print("Cell C2 contains", table.cell("B3"))
+    print("Cell C2 contains", table.cell("C2"))
+"""
+
+
 class Stock:
 
     def __init__(self):
